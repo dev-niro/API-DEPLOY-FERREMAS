66189cff6d062556f0d4f4fc22d0141aafc11079
Nicolás Moya <nirokun23@gmail.com>
2024-06-08 13:56:15 -0400

    arreglando api entrega

--- APIEntrega/Entrega.py
+++ APIEntrega/Entrega.py
@@ -7,26 +7,21 @@ import json
 app = Flask(__name__)
 api = Api(app)
 
-# Lista para almacenar las entregas
 entregas = []
 
-# Clase para manejar las entregas pendientes
+
 class EntregasPendientes(Resource):
-    # Método para obtener todas las entregas pendientes
     def get(self):
         entregas_pendientes = [entrega for entrega in entregas if entrega['estado'] != 'entregada']
         return {'entregas_pendientes': entregas_pendientes}, 200
 
-# Clase para manejar las entregas en proceso de despacho
+
 class EntregasDespacho(Resource):
-    # Método para obtener todas las entregas en proceso de despacho
+    
     def get(self):
         entregas_despacho = [entrega for entrega in entregas if entrega['estado'] == 'despacho']
         return {'entregas_despacho': entregas_despacho}, 200
-
-# Clase para manejar una entrega específica
 class Entrega(Resource):
-    # Método para cambiar el estado de una entrega
     def put(self, id_entrega):
         for entrega in entregas:
             if entrega['id'] == id_entrega:
@@ -34,18 +29,14 @@ class Entrega(Resource):
                 return {'message': 'Estado de la entrega cambiado correctamente'}, 200
         return {'message': 'Entrega no encontrada'}, 404
 
-    # Método para marcar una entrega como realizada
-    def post(self, id_entrega):
-        for entrega in entregas:
-            if entrega['id'] == id_entrega:
-                entrega['estado'] = 'entregada'
-                return {'message': 'Entrega marcada como realizada correctamente'}, 200
-        return {'message': 'Entrega no encontrada'}, 404
 
-# Rutas de la API
+    def post (self):
+        body = request.get_json()
+        entregas.append(body)
+
 api.add_resource(EntregasPendientes, '/entregas')
 api.add_resource(EntregasDespacho, '/entregas/despacho')
-api.add_resource(Entrega, '/entrega/<int:id_entrega>')
+api.add_resource(Entrega, '/entrega')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
